fa57a0d708eaf526d42affc4a653722553f19a8a
johannngansop <johann.ngansop@gmail.com>
2025-10-18 19:36:48 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -19,3 +19,23 @@ variable "ARM_TENANT_ID" {
   description = "Azure tenant ID"
   type        = string
 }
+variable "vnet_name" {
+  description = "Nom du VNet"
+  type        = string
+  default     = "vnet-terraform"
+}
+
+variable "vnet_address_space" {
+  description = "Address space du VNet"
+  type        = list(string)
+  default     = ["10.10.0.0/16"]
+}
+
+variable "subnets" {
+  description = "Subnets du VNet (nom => CIDR)"
+  type        = map(string)
+  default = {
+    "snet-web" = "10.10.1.0/24"
+    "snet-app" = "10.10.2.0/24"
+  }
+}
