0a5105eb1d50a54adb88c6a7ac517ea52de7119a
johannngansop <johann.ngansop@gmail.com>
2025-10-18 21:23:29 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -42,6 +42,33 @@ variable "vnet_address_space" {
   type        = list(string)
 }
 
+# VM Configuration
+variable "vm_name" {
+  description = "Nom de la machine virtuelle"
+  type        = string
+  default     = "vm-johann"
+}
+
+variable "vm_size" {
+  description = "Taille de la VM"
+  type        = string
+  default     = "Standard_B1s"
+}
+
+variable "admin_username" {
+  description = "Nom d'utilisateur administrateur de la VM"
+  type        = string
+  default     = "adminuser"
+}
+
+variable "admin_password" {
+  description = "Mot de passe administrateur"
+  type        = string
+  sensitive   = true
+  default     = "Johann@12345"  # (exemple, tu peux le modifier)
+}
+
+
 variable "subnets" {
   description = "Map of subnet name => CIDR"
   type        = map(string)
