6c79f1686da15f71fbbdee541a907221c0e17f02
johannngansop <johann.ngansop@gmail.com>
2025-10-18 21:21:25 -0400

    Create vm.tf

---
+++ vm.tf
@@ -0,0 +1,37 @@
+# Création d'une interface réseau
+resource "azurerm_network_interface" "vm_nic" {
+  name                = "nic-johann"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnets["web"].id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+# Création de la machine virtuelle
+resource "azurerm_windows_virtual_machine" "vm" {
+  name                = var.vm_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.location
+  size                = var.vm_size
+  admin_username      = var.admin_username
+  admin_password      = var.admin_password
+  network_interface_ids = [azurerm_network_interface.vm_nic.id]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2022-datacenter-azure-edition"
+    version   = "latest"
+  }
+
+  tags = var.tags
+}
